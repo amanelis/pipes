4a21fda951daf6d883fc8d60f8af6600ab883191
Alex Manelis <amanelis@gmail.com>
2012-04-25 00:20:14 -0700

    Big changes to script

--- ruby-rvm.sh
+++ ruby-rvm.sh
@@ -7,35 +7,54 @@ sleep 7
 
 apt-get update
 apt-get upgrade -y
-apt-get install vim -y
+apt-get install git-core -y
 apt-get install wget -y
 apt-get install curl -y
+apt-get install vim -y
 apt-get install ruby -y
-apt-get install build-essential -y
 apt-get install automake -y
-apt-get install git-core -y
+apt-get install build-essential -y
 apt-get install bison -y
 apt-get install openssl -y
 apt-get install libssl-dev -y
-apt-get install readline-common -y
-apt-get install libreadline-dev -y
-apt-get install libreadline6 libreadline6-dev -y
-apt-get install libreadline5 libreadline5-dev -y 
-apt-get install zlib1g zlib1g-dev -y
-apt-get install sqlite3 libsqlite3-0 libsqlite3-dev -y
-apt-get install libmysqlclient-dev -y
-apt-get install libcurl4-openssl-dev -y
-apt-get install libc6-dev ncurses-dev libxml2 libxml2-dev libxslt1-dev libxml2-dev -y
-apt-get install nodejs -y
+apt-get install libssl0.9.8
 apt-get install libqt4-dev -y
 apt-get install libtool -y
 apt-get install libfcgi-dev -y
-apt-get install libyaml-0-2 libyaml-dev -y
+apt-get install libyaml-0-2 -y
+apt-get install libyaml-dev -y
+apt-get install libreadline-dev -y
+apt-get install libreadline6-dev -y
+apt-get install libreadline6 -y
+apt-get install libreadline5-dev
+apt-get install libreadline5 -y 
+apt-get install libsqlite3-0 -y
+apt-get install libsqlite3-dev -y
+apt-get install libmysqlclient-dev -y
+apt-get install libcurl4-openssl-dev -y
+apt-get install libc6-dev -y
+apt-get install libxml2 -y
+apt-get install libxml2-dev -y
+apt-get install libxslt1-dev -y 
+apt-get install libxml2-dev -y
+apt-get install libopenssl-ruby -y
+apt-get install libzlib-ruby -y
+apt-get install libyaml-ruby -y
+apt-get install libdrb-ruby -y 
+apt-get install liberb-ruby -y
+apt-get install nodejs -y
+apt-get install ncurses-dev -y
+apt-get install readline-common -y
+apt-get install sqlite3 -y
+apt-get install zlib1g -y
+apt-get install zlib1g-dev -y
+
+
 
 clear
 echo "************* Configuring RVM ******************"
 apt-get install ruby-rvm -y
 
 rvm pkg install openssl
-rvm install ruby-1.9.2-p180
-
+rvm pkg install readline
+rvm reinstall 1.9.2 --with-openssl-dir=$rvm_path/usr
