bf070765d7997e1125103407ef979d04662f0785
Alex Manelis <amanelis@gmail.com>
2012-04-25 13:15:56 -0700

    RVM

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@ echo "-----> Installing system wide dependencies"
 bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/dependencies.sh )
 
 echo "-----> Installing rvm"
-bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/rvm.sh )
---
+++ scripts/rvm.sh
@@ -0,0 +1,7 @@
+bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
+
+wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
+
+source .bashrc
+
+rvm install 1.9.2-head
