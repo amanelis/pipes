ce4d44d1eb09b33cdbd4219354d19dd8d7f2b456
Alex Manelis <amanelis@gmail.com>
2012-04-25 15:29:16 -0700

    Changed bashrc

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -2,7 +2,7 @@ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscr
 
 wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
 
-source /root/.bashrc
+source ~/.bashrc
 bash --login
 
 type rvm | head -n 1
