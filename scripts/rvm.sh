4cadd0b5112fa28367351cb2a0d668fcc52b29ef
Alex Manelis <amanelis@gmail.com>
2012-04-25 14:44:30 -0700

    Badass

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -2,8 +2,7 @@ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscr
 
 wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
 
-bash
-source /root/.bashrc
+bash --login
 
-rvm install 1.9.2-head
-rvm --default 1.9.2-head
+/usr/local/rvm/bin/rvm install 1.9.2-head
+/usr/local/rvm/bin/rvm --default 1.9.2-head
