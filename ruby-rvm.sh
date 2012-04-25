61224d7c67973b53dfacf3d303564cb7b63f880d
Alex Manelis <amanelis@gmail.com>
2012-04-24 23:59:57 -0700

    Trying the patch

--- ruby-rvm.sh
+++ ruby-rvm.sh
@@ -7,24 +7,33 @@ sleep 7
 
 apt-get update
 apt-get upgrade -y
-apt-get install ruby wget -y
+apt-get install vim -y
+apt-get install wget -y
+apt-get install curl -y
+apt-get install ruby -y
+apt-get install build-essential -y
+apt-get install automake -y
 apt-get install git-core -y
-apt-get install build-essential automake bison openssl libreadline5 libreadline5-dev curl zlib1g zlib1g-dev libssl-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libmysqlclient-dev vim -y
+apt-get install bison -y
+apt-get install openssl -y
+apt-get install libssl-dev -y
+apt-get install readline-common -y
+apt-get install libreadline6 libreadline6-dev -y
+apt-get install libreadline5 libreadline5-dev -y 
+apt-get install zlib1g zlib1g-dev -y
+apt-get install sqlite3 libsqlite3-0 libsqlite3-dev -y
+apt-get install libmysqlclient-dev -y
 apt-get install libcurl4-openssl-dev -y
-apt-get install libxml2 libxml2-dev libxslt1-dev -y
+apt-get install libxml2 libxml2-dev libxslt1-dev libxml2-dev -y
 apt-get install nodejs -y
 apt-get install libqt4-dev -y
-apt-get install readline-common -y
 apt-get install libtool -y
 apt-get install libfcgi-dev -y
-apt-get install libyaml-0-2 -y
+apt-get install libyaml-0-2 libyaml-dev -y
 
 clear
 echo "************* Configuring RVM ******************"
 apt-get install ruby-rvm -y
 
-rvm install 1.8.7
-rvm install 1.9.2
-rvm install 1.9.3
-
+rvm install ruby-1.9.2-p180 --patch openssl.patch
 
