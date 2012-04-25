c799983a03f8c0d7cc1e858a547b6ba663e5bccd
Alex Manelis <amanelis@gmail.com>
2012-04-25 13:25:26 -0700

    rvm install

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -2,6 +2,4 @@ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscr
 
 wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
 
-#source .bashrc
-
-#rvm install 1.9.2-head
+rvm install 1.9.2-head
