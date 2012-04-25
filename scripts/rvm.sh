47901774faed9461438a0641772f3b9d90f08c74
Alex Manelis <amanelis@gmail.com>
2012-04-25 13:43:50 -0700

    RVM is working

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -3,3 +3,4 @@ bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscr
 wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
 
 rvm install 1.9.2-head
+rvm --default 1.9.2-head
