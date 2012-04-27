e76b4de90a4dcb8e85482f235c82a591817f1d01
Alex Manelis <amanelis@gmail.com>
2012-04-27 14:56:59 -0700

    Added a dotfile

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -1,6 +1,7 @@
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 
 wget https://raw.github.com/amanelis/pipes/master/lib/bashrc -O .bashrc
+wget https://raw.github.com/amanelis/pipes/master/lib/gemrc -O .gemrc
 
 source ~/.bashrc
 bash --login
