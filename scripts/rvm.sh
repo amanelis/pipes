07db400c9d9c3b3e97bb8cfcabe1abe896598c09
Alex Manelis <amanelis@gmail.com>
2012-04-28 13:30:30 -0700

    Added versions to be safer

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -9,4 +9,8 @@ bash --login
 rvm install ruby-1.9.2-p320
 rvm --default use 2.9.2-p320
 
+
+gem install passenger -v '3.0.12'
+gem install rails -v '3.1.3'
+
 # rvmsudo passenger-install-nginx-module
