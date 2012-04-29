e97f0f59c25cf1fd736fb52d455a77a0db58b5e5
Alex Manelis <amanelis@gmail.com>
2012-04-28 19:51:07 -0700

    Added the correct config file in

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -11,19 +11,12 @@ bash --login
 rvm install ruby-1.9.2-p320
 rvm --default use 2.9.2-p320
 
-
 gem install passenger -v '3.0.12'
 gem install rails -v '3.1.3'
 
-
-
 echo "mysql-server mysql-server/root_password select (password omitted)" | debconf-set-selections;
 echo "mysql-server mysql-server/root_password_again select (password omitted)" | debconf-set-selections;
-
 apt-get -y install mysql-server
 
 passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download
-
-# rvmsudo passenger-install-nginx-module
-# OR
-# /usr/local/rvm/gems/ruby-1.9.2-p320/bin/passenger-install-nginx-module
+wget https://raw.github.com/amanelis/pipes/master/lib/nginx.conf -O /opt/nginx/conf/nginx.conf
