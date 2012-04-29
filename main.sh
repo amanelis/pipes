3cd0d3759bdccc1f7c0920d6a0c0e0cc164c2006
Alex Manelis <amanelis@gmail.com>
2012-04-29 00:09:45 -0700

    Added to main, ready to run this

--- main.sh
+++ main.sh
@@ -1,15 +1,11 @@
 echo "-----> Installing system wide dependencies"
 bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/essentials.sh )
 
-# echo "-----> Installing rvm"
-# bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/rvm.sh )
-
 echo "-----> Installing Ruby 1.9.3"
 bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/ruby193.sh )
 
 echo "-----> Installing MySQL Server"
 bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/mysql.sh )
-
 
 echo "-----> Installing Nginx Passenger"
 bash -s stable < <(curl -s https://raw.github.com/amanelis/pipes/master/scripts/nginx.sh )
