4149d80f15ee056d6148022252ac55d566e3c844
Alex Manelis <amanelis@gmail.com>
2012-04-28 23:42:35 -0700

    mysql passwrod

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "mysql-server mysql-server/root_password select (password omitted)" | debconf-set-selections;
-echo "mysql-server mysql-server/root_password_again select (password omitted)" | debconf-set-selections;
+echo mysql-server mysql-server/root_password select changeme | debconf-set-selections
+echo mysql-server mysql-server/root_password_again select changeme | debconf-set-selections
+
 apt-get -y install mysql-server
