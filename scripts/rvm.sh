1d6be63a68c715e8625b419cc306074f633b9c24
Alex Manelis <amanelis@gmail.com>
2013-02-01 14:05:41 -0800

    Fixed type in use statement

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -3,4 +3,4 @@
 bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )
 
 rvm install ruby-1.9.2-p320
-rvm --default use 2.9.2-p320
+rvm --default use 1.9.2-p320
