b9a4894e8be33dc6d0221bdeca9729b363e8b92c
Alex Manelis <amanelis@gmail.com>
2016-01-10 17:24:20 -0500

    Small typo caused whole thing to crash

--- main.sh
+++ main.sh
@@ -1,24 +1,20 @@
 echo "-----> Installing system wide dependencies"
 wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/essentials.sh
 
-
 echo "-----> Installing Ruby 2.2.3"
-wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/ruby223.sh
-
+wget -O /tmp/ruby223.sh https://raw.github.com/amanelis/pipes/master/scripts/ruby223.sh
 
 echo "-----> Installing MySQL Server"
-wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/mysql.sh
-
+wget -O /tmp/mysql.sh https://raw.github.com/amanelis/pipes/master/scripts/mysql.sh
 
 echo "-----> Installing Postgresql Server"
-wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/postgres.sh
-
+wget -O /tmp/postgres.sh https://raw.github.com/amanelis/pipes/master/scripts/postgres.sh
 
 echo "-----> Installing Nginx Passenger"
-wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/nginx.sh
+wget -O /tmp/nginx.sh https://raw.github.com/amanelis/pipes/master/scripts/nginx.sh
 
 
 echo "-----> Setting up users and groups"
-wget -O /tmp/essentials.sh https://raw.github.com/amanelis/pipes/master/scripts/user.sh
+wget -O /tmp/user.sh https://raw.github.com/amanelis/pipes/master/scripts/user.sh
 
 bash /tmp/essentials.sh && bash /tmp/ruby223.sh && bash /tmp/mysql.sh && bash /tmp/postgres.sh && bash /tmp/nginx.sh && bash /tmp/user.sh
