776e6bbab55c0bcd6ea7e8b7613f639a006bd48c
Alex Manelis <amanelis@gmail.com>
2016-01-10 21:33:29 -0500

    Typo on filename

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -8,11 +8,13 @@ sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty m
 apt-get -y update
 apt-get install -y nginx-extras passenger
 
+wget https://raw.github.com/amanelis/pipes/master/lib/nginx.conf -O /etc/nginx/nginx.conf
+
 # gem install passenger -v '5.0.21'
 # gem install rails -v '4.2.4'
 #
 # passenger-install-nginx-module --auto --prefix=/opt/nginx --auto-download
-wget https://raw.github.com/amanelis/pipes/master/lib/nginx.conf -O /opt/nginx/conf/nginx.conf
+# wget https://raw.github.com/amanelis/pipes/master/lib/nginx.conf -O /opt/nginx/conf/nginx.conf
 
 # wget https://raw.github.com/amanelis/pipes/master/lib/nginx -O /etc/init.d/nginx
 # chmod a+x /etc/init.d/nginx
